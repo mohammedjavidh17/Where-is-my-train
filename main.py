7ea7a3a2cce78a39674d67111db41bac71a0ebe3
Javidh <98268633+mohammedjavidh17@users.noreply.github.com>
2022-06-23 23:32:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import numpy as np
+import pandas as pd
 
 root = requests.get('http://www.chennailocaltrain.com/chengalpattu-to-beach-train-timings-1.html').text
 soup = BeautifulSoup(root, 'lxml')
@@ -12,3 +14,17 @@ for x in dayS:
     b.remove("DAYS")
     for y in b:
         day.append(y)
+data = soup.find_all('table', height=["822", "862", '842'])
+
+for i,d in enumerate(data):
+    dataLst = []
+    val = str(d.text)
+    val = val.replace(' ', '')
+    val = val.split()
+    for x in range(0, len(val), 8):
+        if i>1:
+            x = x+1
+        dataLst.append(val[15+x:15+8+x])
+    df = pd.DataFrame(dataLst).iloc[:-2, :]
+    df = df.replace('--', np.nan)
+    print(df)
