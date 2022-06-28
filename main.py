2735b6f2f20029d9fcc0002326fa0b3e919d540c
Javidh <98268633+mohammedjavidh17@users.noreply.github.com>
2022-06-28 09:13:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import numpy as np
 import pandas as pd
 
-root = requests.get('http://www.chennailocaltrain.com/chengalpattu-to-beach-train-timings-1.html').text
+root = requests.get('http://www.chennailocaltrain.com/chengalpattu-to-beach-train-timings-2.html').text
 soup = BeautifulSoup(root, 'lxml')
 dayS = soup.find_all('tr', bgcolor="#804000")
 day = []
@@ -14,17 +14,21 @@ for x in dayS:
     b.remove("DAYS")
     for y in b:
         day.append(y)
-data = soup.find_all('table', height=["822", "862", '842'])
+data = soup.find_all('table')
 
 for i,d in enumerate(data):
     dataLst = []
     val = str(d.text)
     val = val.replace(' ', '')
     val = val.split()
-    for x in range(0, len(val), 8):
-        if i>1:
-            x = x+1
-        dataLst.append(val[15+x:15+8+x])
-    df = pd.DataFrame(dataLst).iloc[:-2, :]
-    df = df.replace('--', np.nan)
-    print(df)
+    if len(val) > 238:
+        buf = []
+        buf.append(val[-224:])
+        val = buf[0]
+        for x in range(0, len(val), 8):
+            
+            dataLst.append(val[x:8+x])
+        df = pd.DataFrame(dataLst)
+        df = df.replace('--', np.nan)
+        print(df)
+        
