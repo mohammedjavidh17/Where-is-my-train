806dec19fc0cf97a843740df024b8ad52207178f
Javidh <98268633+mohammedjavidh17@users.noreply.github.com>
2022-06-29 21:25:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 
 testLink = [224,238,'http://www.chennailocaltrain.com/beach-to-chengalpattu-train-timings-1.html']
+testLink1 = [224,238,'http://www.chennailocaltrain.com/beach-to-chengalpattu-train-timings-1.html']
+testLink2 = [144,157,'http://www.chennailocaltrain.com/tambaram-to-beach-train-timings-1.html']
 def get(siz):
     root = requests.get(siz[2]).text
     soup = BeautifulSoup(root, 'lxml')
@@ -14,21 +16,24 @@ def get(siz):
         val = str(d.text)
         val = val.replace(' ', '')
         val = val.split()
-        if len(val) > siz[0]:
+        print(len(val))
+        if len(val) > siz[1]:
             buf = []
             run = []
-            run.append(val[-siz[0]-6:-siz[0]])
-            buf.append(val[-siz[0]:])
-            val = buf[0]
-            for x in range(0, len(val), 8):
-                
-                dataLst.append(val[x:8+x])
-            df = pd.DataFrame(dataLst).iloc[:, 1:-1]
-            df = df.replace('--', np.nan)
-            df = pd.concat([pd.DataFrame(run, columns=list(range(1,7))), df])
-            #print(df)
-            Datadf = pd.concat([Datadf, df], axis=1)
+            
+            if True:
+                run.append(val[-siz[0]-6:-siz[0]])
+                buf.append(val[-siz[0]:])
+                val = buf[0]
+                for x in range(0, len(val), 8):
+                    
+                    dataLst.append(val[x:8+x])
+                df = pd.DataFrame(dataLst).iloc[:, 1:-1]
+                df = df.replace('--', np.nan)
+                df = pd.concat([pd.DataFrame(run, columns=list(range(1,7))), df])
+                #print(df)
+                Datadf = pd.concat([Datadf, df], axis=1)
     return Datadf
-print(get(testLink))
+get(testLink2).to_csv('assets\\test.csv')
 
         
